ad1fba8648afcf9318a7eb2bc68161921f1d4b51
Vinicius Tatsumi Hatori Teles <62632221+Viniciustatsumi@users.noreply.github.com>
2025-07-01 01:57:11 -0300

    Add files via upload

---
+++ lista_atividade3/atividade1.java
@@ -0,0 +1,11 @@
+package lista_atividade3;
+
+public class atividade1 {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i<=20; i++){
+            System.out.println("Eu Amo estudar Algoritmos!");
+        }
+    }
+}
