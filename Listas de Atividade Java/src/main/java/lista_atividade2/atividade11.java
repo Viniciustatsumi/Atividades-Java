101208b14664910e85b571677a81af44bc054371
Vinicius Tatsumi Hatori Teles <62632221+Viniciustatsumi@users.noreply.github.com>
2025-07-01 00:03:40 -0300

    Add files via upload

---
+++ Listas de Atividade Java/src/main/java/lista_atividade2/atividade11.java
@@ -0,0 +1,39 @@
+package lista_atividade2;
+
+import java.util.Scanner;
+
+public class atividade11 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Olá, seja bem vindo aos Calçados do Japa!");
+        System.out.println("É um prazer ter você aqui com a gente!");
+        System.out.println("Vamos ver quanto que você vendeu esse mês?");
+        System.out.println("--------------------------------------------------");
+
+        System.out.println("Mas para começar, qual seu nome?");
+        String nome = entrada.nextLine();
+
+        System.out.println(nome + " qual o seu salário fixo?");
+        double salario = entrada.nextDouble();
+
+        System.out.println(nome + " quantas vendas você realizou esse mês em dinheiro? (R$)");
+        double vendas = entrada.nextInt();
+
+        double comissao = vendas * 0.15;
+        double salarioFinal = salario + comissao;
+
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("------------- RELÁTORIO DO VENDEDOR------------------");
+        System.out.println("Funcionário: " + nome);
+        System.out.printf("Sálario: R$ %.2f%n", salario);
+        System.out.printf("Vendas: R$ %.2f%n", vendas);
+        System.out.printf("Comissao: R$ %.2f%n", comissao);
+        System.out.println("------------------------------");
+        System.out.printf("Salário final: R$ %.2f%n", salarioFinal);
+
+    }
+}
